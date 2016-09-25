394403e4ad60de6e19025d3aa444c7463e57468c
LiliwoL <contact_github@liliwol.fr>
2016-09-25 18:13:54 +0200

    First commit

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -9,7 +9,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Project name</a>
+					<a class="navbar-brand" href="#">Formation GIT</a>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
